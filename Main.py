269f5c510603628f49b42792741a86630ab79210
mardaren <mardaren1907@gmail.com>
2020-07-15 22:56:12 +0300

    datamanipulator I

--- Main.py
+++ Main.py
@@ -3,16 +3,10 @@
 # class DataManager:
 #     training_test_percentage = 0.0
 from Model import LR
-from DataManipulater import Manager
+from datamanipulator import Manager
 from HannesTool import HannesTool
 class Main:
     # init
-    
-    data_manager = Manager(25)
-    model = LR(data_manager.get_number_of_pieces())
-    tool = HannesTool()
-    W = model.miniBatchGradientDescent(data_manager)
-    print("W:", W)
-    print("Error:",tool.get_err('Class', W))
+    print()
 
 
---
+++ datamanipulator.py
@@ -0,0 +1,38 @@
+"""csv ile uğraşılıp verinin alındığı modül"""
+import pandas as pd
+######################
+####Constants#########
+######################
+DATA_DEST = "FinancialForecast\\Data\\BIST_100_Gecmis_Verileri_Haftalik.csv"
+TOTAL_LENGTH = pd.read_csv(DATA_DEST).shape[0]
+TR_DATA_LENGTH, TE_DATA_LENGTH = 16, 8
+
+
+class Manager:
+    """datamanipulatorun main kısmı"""
+    def __init__(self):  # constructor
+        self.data_training, self.data_test = self.prepare_data()
+
+
+    def prepare_data(self):
+        """Datayı oluşturuyor"""
+        data_tr, data_te = [], []
+        bound = TR_DATA_LENGTH + TE_DATA_LENGTH - 2                         #tr-te sınırını belirleyen yer
+        data = pd.read_csv(DATA_DEST)
+        for row_index in range(TOTAL_LENGTH - 1):                           #sonuncu zaten kullanilamaz
+            row_now = data.iloc[row_index]
+            target = data["Fark"].iloc[row_index + 1]
+            piece_now = list(row_now.iloc[8:])
+            piece_now.append(target)
+            if row_index % bound >= 0 and row_index % bound < 16:
+                data_tr.append(piece_now)
+            elif row_index % bound >= 16 and row_index % bound < bound:
+                data_te.append(piece_now)
+        return data_tr, data_te
+
+    def plot(self):
+        """Datayı çiziyor"""
+        data = pd.read_csv(DATA_DEST)
+        data.iloc[:, 7:].plot()
+
+Manager()
