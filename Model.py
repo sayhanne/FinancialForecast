01e6c30504ca3707f1ebf2baa9a4c1db0057d70e
sayhanne <sayhannee@gmail.com>
2020-07-15 20:00:55 +0300

    Lineer Regression has been updated to  Multiple Lineer Regression

--- Model.py
+++ Model.py
@@ -1,35 +1,33 @@
-from HannesTool import HannesTool
-# from DataManipulater import Manager
+# from HannesTool import HannesTool
 import random
-import statistics
+# import statistics
 
-class DataCollector:
 
-    x = None
-    y = None
-    manager_object = None
+# class DataCollector:
+#
+#     def __init__(self, manager):
+#         self.x = None
+#         self.y = None
+#         self.manager_object = manager
+#         self.set_next_piece()
+#
+#     def set_next_piece(self):  # must return X matrix and Y array
+#         self.x = []
+#         self.y = []
+#         for piece in self.manager_object.get_training_piece():
+#             self.x.append(piece[0])
+#             self.y.append(piece[1])
+#
+#     def get_current_piece(self):
+#         return self.x, self.y
 
-    def __init__(self, manager):
-        global x, y, manager_object
-        manager_object = manager
-        self.set_next_piece()
-
-    def set_next_piece(self):
-        global x, y
-        x = []
-        y = []
-        for piece in manager_object.get_training_piece():
-            x.append(piece[0])
-            y.append(piece[1])
-
-    def get_current_piece(self):
-        return x,y
 
 class LR:
-    # reader = None
 
     def __init__(self, number):  # constructor
-        self.parameters = [0.5, 0.5]
+        self.weightArrays = []
+        self.numberOfPredictors = 0
+        self.parameters = []
         self.target = []
         self.managerObject = None
         self.estimation = []
@@ -38,77 +36,65 @@ class LR:
         self.batch_number = number
 
     def initializeParameters(self):
-        self.parameters[1] = random.random()/10
-        self.parameters[0] = statistics.mean(self.data) * random.random() * 2
-
-    def estimationCalculator(self):
-        for i in range(len(self.target)):
-            self.estimation[i] = (self.estimationForRow(i))
-        return
-
-    def estimationForRow(self, row):
-        return self.parameters[0] + self.data[row] * self.parameters[1]
-
-    def costFunction(self, predictor):  # cost function is mse
-        self.estimationCalculator()
-        cost = 0
-        for i in range(len(self.data)):
-            if predictor == 0:
-                cost += (self.target[i] - self.estimation[i]) * -1
-            else:
-                cost += (self.target[i] - self.estimation[i]) * -self.data[i]
-        return 2 * cost / len(self.data)
+        arr = []
+        for i in range(self.numberOfPredictors + 1):  # adding initial intercept and weights of predictors
+            arr.append(random.random())
+        return arr
 
     def initializeEstimation(self):
         arr = []
         for i in range(len(self.target)):
-            arr.append(None)
+            arr.append(0.0)
         return arr
 
-    def mseForDataPiece(self):
-        total = 0.0
-        for i in range(len(self.target)):
-            total += (self.target[i] - self.estimation[i]) ** 2
-        return total / len(self.target)
+    def estimationForRow(self, row):
+        estimation = self.parameters[0]  # first the intercept is added
+        for i in range(len(self.parameters) - 1):
+            estimation += self.parameters[i+1] * self.data[row][i]   # then all the remaining is added
+        return estimation
 
-    def miniBatchGradientDescent(self, manager):
+    def estimationCalculator(self):
+        for i in range(len(self.target)):
+            self.estimation[i] = self.estimationForRow(i)
+        return
+
+    def costFunction(self, predictor):  # cost function is mse
+        self.estimationCalculator()
+        cost = 0.0
+        for i in range(len(self.data)):
+            if predictor == 0:
+                cost += (self.target[i] - self.estimation[i]) * -1
+            else:
+                cost += (self.target[i] - self.estimation[i]) * -self.data[i][predictor-1]
+        return 2 * cost / len(self.data)
+
+    def miniBatchGradientDescent(self, manager, numOfPredictors):
+        self.numberOfPredictors = numOfPredictors  # number of predictors can change
         self.managerObject = manager
-        initialized = False
-        index = 1
-        ina=0
-        mse = 0.0
         converged = False
-        tool = HannesTool()
+        index = 0
+        count = 0
+        self.parameters = self.initializeParameters()
         while not converged:
             self.data, self.target = self.getDataPiece()
             self.estimation = self.initializeEstimation()
-            if not initialized:
-                self.initializeParameters()
-                initialized = True
-                #print("W's first:", self.parameters)
-                for i in range(len(self.data)):
-                    print(self.data[i]*self.parameters[1]+self.parameters[0], end=' ')
-                print()
-                
-            for j in range(2):
-                cost = self.costFunction(j)
-                #print("bgd",j,"parametreler:",self.parameters[j], "cost:",cost)
-                self.parameters[j] = self.parameters[j] - self.learning_rate * cost
-
-
-            index = index + 1
-            if index > self.batch_number:
-                index = 1
-                # mse = tool.get_err("R2", self.parameters)
-                mse = self.mseForDataPiece()
-                if(ina<150):
-                    ina+=1
+            for i in range(numOfPredictors + 1):
+                self.parameters[i] = self.parameters[i] - self.learning_rate * self.costFunction(i)
+            index += 1
+            if index == self.batch_number:
+                index = 0
+                count += 1
+                if count == 150:
+                    converged = True
+                    self.weightArrays.append(self.parameters)
+                    break
                 else:
-                    return self.parameters
+                    continue
+            else:
+                continue
+        return
 
-
-
-    def getDataPiece(self):
+    def getDataPiece(self):  # must return X matrix and Y array
         x = []
         y = []
         for piece in self.managerObject.get_training_piece():
