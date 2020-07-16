ad5b6cf671187499cb661612783d65bca510b4b9
mardaren <mardaren1907@gmail.com>
2020-07-16 14:05:19 +0300

    hannestool restored

--- HannesTool.py
+++ HannesTool.py
@@ -1,55 +1,68 @@
-import pandas as pd
-import numpy as np
+"""Yardımcı Toollar için kullanılan bir modül"""
 
-data_dest="Data\\data_for_model.csv"
+
 class HannesTool:
-    def get_mean(self, Y):
-        tot = 0
-        for i in range(0, len(Y)):
-            tot += Y[i]
-        return tot / len(Y)
+    """Tek classı"""
 
-    def getTSS(self, Y):
-        mean = self.get_mean(Y)
+    def get_mean(self, target):
+        """returns mean of target"""
+        tot = 0
+        for i in enumerate(target):
+            tot += target[i]
+        return tot / len(target)
+
+    def get_tss(self, data_test):
+        """returns TSS of target"""
+        targets = []
+        for _ in enumerate(data_test):
+            targets.append(data_test[-1])
+        mean = self.get_mean(targets)
         tss = 0
-        for i in range(0, len(Y)):
-            tss += (Y[i] - mean) ** 2
-        #print("tss:",tss)
+        for target in targets:
+            tss += (target - mean) ** 2
         return tss
 
-    def getRSS(self, fx, Y):
-        rss = 0
-        for i in range(0, len(Y)):
-            rss += (Y[i] - fx[i]) ** 2
-        #print("rss:",rss)
+    def get_rss(self, weights, data_test):
+        """returns RSS of model"""
+        rss, tss = 0, 0
+        for test_sample in data_test:
+            # estimation yani fx oluşturuluyor
+            estimation = 0
+            for w_index in enumerate(weights):
+                if w_index == 0:
+                    estimation = weights[w_index]
+                else:
+                    estimation += test_sample[w_index - 1] * weights[w_index]
+            #rss hesap kısmı
+            target = test_sample[-1]
+            tss += self.get_tss(target)
+            rss += (target - estimation) ** 2
         return rss
 
-    def getR2(self, fx, Y):
-        return 1 - self.getRSS(fx, Y) / self.getTSS(Y)
+    def get_r2(self, weights, data_test):
+        """returns R2 of model"""
+        return 1 - self.get_rss(weights, data_test) / self.get_tss(data_test)
 
-    def to_X_Y(self, data):
-        arr=[]
-        for i in range(0,len(data)-1):
-            arr.append([data[i],data[i+1]])
-        return arr
-    
-    def get_err(self,type,W):
-        test_x=np.array(pd.read_csv(data_dest)["te_now"].dropna())
-        test_y=np.array(pd.read_csv(data_dest)["te_next"].dropna())
-        fx=test_x*W[1]+W[0]
-        
-        if(type=='R2'):
-            return self.getR2(fx, test_y)
-        elif(type=='Class'):
-            return self.get_err_class(fx, test_y)
-    
-    def get_err_class(self,fx,Y):
-        total=0
-        for i in range(len(Y)):
-            if(Y[i]*fx[i]>0):
-                print("s", Y[i], fx[i])
-                total+=1
-        print("total/len",total, len(Y))
-        return total/len(Y)
+    def get_err(self, error_type, weights, data_test):
+        """returns error of model according to given type (data_test denilen şey 1 paket)"""
+        if error_type == 'R2':
+            return self.get_r2(weights, data_test)
+        elif error_type == 'Class':
+            return self.get_err_class(weights, data_test)
 
-
+    def get_err_class(self, weights, data_test):
+        """tahminimizin class olarak doğruluk oranını hesaplıyor"""
+        total = 0
+        for test_sample in data_test:
+            # estimation yani fx oluşturuluyor
+            estimation = 0
+            for w_index in enumerate(weights):
+                if w_index == 0:
+                    estimation = weights[w_index]
+                else:
+                    estimation += test_sample[w_index - 1] * weights[w_index]
+            #### hesap kısmı
+            target = test_sample[-1]
+            if target * estimation > 0:
+                total += 1
+        return total/len(data_test)
